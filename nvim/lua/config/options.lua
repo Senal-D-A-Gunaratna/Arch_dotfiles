12cee96e52fefc39ad680e049415a4adaf81c75c
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-04-15 13:44:22 +0530

    updated kitty colourpalate

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Disable True Color support so it falls back to your terminal (Kitty)
+-- Disable True Color support so it falls back to your terminal
 vim.opt.termguicolors = false
 -- Ensure Neovim knows your terminal supports at least 256 colors for compatibility
 -- but we will limit the theme to 16.
