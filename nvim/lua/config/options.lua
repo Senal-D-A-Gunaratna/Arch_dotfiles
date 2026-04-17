f6b0da8e9f68c6aefe1fc043f389f062b6136bbe
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-04-17 10:33:21 +0530

    feat: add modular ANSI colorscheme with auto-override for neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- Disable True Color support so it falls back to your terminal
-vim.opt.termguicolors = false
--- nvim/lua/plugins/terminal-theme.lua
+++ nvim/lua/plugins/terminal-theme.lua
@@ -1,17 +1,22 @@
 return {
-  -- 1. Disable the default LazyVim theme loading
+  -- 1. Force LazyVim to use your ANSI theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function() end, -- This stops LazyVim from forcing a theme
+      colorscheme = "ansi",
     },
   },
 
-  -- 2. Use a theme that strictly follows terminal ANSI colors
+  -- 2. The ANSI theme plugin with the automatic override
   {
     "stevedylandev/ansi-nvim",
     lazy = false,
     priority = 1000,
+    init = function()
+      -- This overrides your options.lua automatically [cite: 17, 20]
+      vim.opt.termguicolors = false 
+      vim.o.background = "dark" 
+    end,
     config = function()
       vim.cmd([[colorscheme ansi]])
     end,
