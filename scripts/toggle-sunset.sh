f7d629cdc13c7554ed006f119cf1e5f6fffb7f43
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-04-20 13:38:39 +0530

    added dunst notification

--- scripts/toggle-sunset.sh
+++ scripts/toggle-sunset.sh
@@ -2,6 +2,8 @@
 
 if pgrep -x "hyprsunset" > /dev/null; then
     pkill -x "hyprsunset"
+    dunstify -u low -t 2000 "Hyprsunset" "Blue light filter disabled"
 else
     hyprsunset &
+    dunstify -u low -t 2000 "Hyprsunset" "Blue light filter enabled"
 fi
