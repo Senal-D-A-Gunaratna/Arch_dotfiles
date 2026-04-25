a042fd3be0192b47148e09816347c0ceea277709
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-04-25 13:36:36 +0530

    hyprsunset message

--- scripts/toggle-sunset.sh
+++ scripts/toggle-sunset.sh
@@ -2,8 +2,8 @@
 
 if pgrep -x "hyprsunset" > /dev/null; then
     pkill -x "hyprsunset"
-    notify-send -u low -t 2000 "Hyprsunset" "Filter Disabled"
+    notify-send -u low -t 2000 "Hyprsunset" "Blue light filter Disabled"
 else
     hyprsunset &
-    notify-send -u low -t 2000 "Hyprsunset" "Filter Enabled"
+    notify-send -u low -t 2000 "Hyprsunset" "Blue light filter Enabled"
 fi
