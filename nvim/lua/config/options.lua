46512790a1df9a87208ea0162e0bb33c96a56c7a
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-05-07 11:46:17 +0530

    feat(nvim): install additional LSPs and tweak mouse scrolling
    - Install css-lsp for CSS/SCSS/Less support
    - Install bash-language-server for shell scripts
    - Set mousescroll to ver:1 for smoother mouse navigation

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+--Snacks explorer mousescroll set to 1 line pre tick
+vim.opt.mousescroll = "ver:1,hor:6"
