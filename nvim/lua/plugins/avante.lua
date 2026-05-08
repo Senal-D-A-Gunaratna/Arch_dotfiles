498f4b9b895ab3ba574548582db583b0e72fb8c4
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-05-08 17:49:51 +0530

    removed claude

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -23,15 +23,6 @@ return {
     provider = "gemini",
 
     providers = {
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-sonnet-4-20250514",
-        timeout = 30000,
-        extra_request_body = {
-          temperature = 0.75,
-          max_tokens = 20480,
-        },
-      },
       moonshot = {
         endpoint = "https://api.moonshot.ai/v1",
         model = "kimi-k2-0711-preview",
