0376bfa3ca69a56bb4127bdb948d5b6b32ad637f
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-05-14 14:20:28 +0530

    revert to stable .conf

--- matugen/templates/hyprland-colors.lua
+++
@@ -1,8 +0,0 @@
-return {
-    active_border = "rgb({{colors.primary.default.hex_stripped}})",
-    wall = "{{image}}",
-    primary_3_rgba = "rgb({{colors.primary_container.default.hex_stripped | lighten: 15.0 }})",
-    p1_accent_4_rgba = "rgb({{colors.primary_container.default.hex_stripped}})",
-    p3_accent_6_rgba = "rgb({{colors.primary.default.hex_stripped | lighten: -10.0 }})",
-    dot_color = "rgb({{colors.primary.default.hex_stripped | lighten: -10.0 }})",
-}
