510c659dd50f6492a8634aae6fd12dd9c266d9d4
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-05-23 19:30:58 +0530

    feat: disable tokyonight and catppuccin default plugins

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  -- Disable tokyonight (LazyVim default)
+  { "folke/tokyonight.nvim", enabled = false },
+  -- Disable catppuccin (LazyVim default)
+  { "catppuccin/nvim", name = "catppuccin", enabled = false },
+}
