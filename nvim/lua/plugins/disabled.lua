452edeb55933a2331c36dd770876ff1cf1d5e055
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-05-24 17:44:20 +0530

    fix: override LazyVim colorscheme to matugen

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
+  { "LazyVim/LazyVim", opts = { colorscheme = "matugen" } },
   -- Disable tokyonight (LazyVim default)
   { "folke/tokyonight.nvim", enabled = false },
   -- Disable catppuccin (LazyVim default)
