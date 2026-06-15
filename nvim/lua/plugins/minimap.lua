5b696faab12cb8dbc1c73af7b16746223657134b
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-06-15 22:20:16 +0530

    nvim: mini map

--- nvim/lua/plugins/minimap.lua
+++ nvim/lua/plugins/minimap.lua
@@ -1,15 +1,16 @@
 return {
   {
-    "Isrothy/neominimap.nvim", -- Fixed repository name
+    "Isrothy/neominimap.nvim",
     version = "v3.*",
     enabled = true,
     lazy = false,
     keys = {
-      -- Fixed command capitalization
       { "<leader>uM", "<cmd>Neominimap Toggle<cr>", desc = "Toggle Minimap" },
     },
     init = function()
-      vim.g.neominimap_singleton = true
+      vim.g.neominimap = {
+        auto_enable = false,
+      }
     end,
   },
 }
