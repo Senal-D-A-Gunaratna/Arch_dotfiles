ab273a014969e66aaf621a19ab5311a87e7a17eb
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-06-15 21:49:15 +0530

    base

--- nvim/lua/plugins/opts.lua
+++ nvim/lua/plugins/opts.lua
@@ -7,4 +7,27 @@ return {
       },
     },
   },
-}
+
+  {
+  "supermaven-inc/supermaven-nvim",
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<Tab>", -- Hits Tab to accept the ghost text
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+      },
+    }),
+  },
+{                                                                          
+
+  {"mbbill/undotree",                                   
+  cmd = "UndotreeToggle",                                                  
+  keys = {                                                                 
+    { "<leader>uu", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" }, 
+  },                                                                       
+  config = function()                                                      
+    -- Optional: Persistent undo so history survives restarts              
+    vim.opt.undofile = true                                                
+  end,                                                                     
+},                                                                         
