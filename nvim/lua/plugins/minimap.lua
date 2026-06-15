b3da474ca9a7a22c7037ea07064c2bd202efd199
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-06-15 22:17:15 +0530

    minimap

--- nvim/lua/plugins/minimap.lua
+++ nvim/lua/plugins/minimap.lua
@@ -1,30 +1,15 @@
 return {
   {
-    -- FIXED: Changed from 'echasnovski/mini.map' to match the new repository structure
-    "nvim-mini/mini.map",
-    version = false,
+    "Isrothy/neominimap.nvim", -- Fixed repository name
+    version = "v3.*",
+    enabled = true,
+    lazy = false,
     keys = {
-      { "<leader>uM", "<cmd>lua MiniMap.toggle()<cr>", desc = "Toggle Minimap" },
+      -- Fixed command capitalization
+      { "<leader>uM", "<cmd>Neominimap Toggle<cr>", desc = "Toggle Minimap" },
     },
-    opts = function()
-      local map = require("mini.map")
-      return {
-        integration = {
-          map.gen_integration.builtin_search(),
-          map.gen_integration.gitsigns(),
-          map.gen_integration.diagnostic(),
-        },
-        symbols = {
-          encode = map.gen_encode_symbols.dot("2x2"),
-        },
-        window = {
-          side = "right",
-          width = 12,
-        },
-      }
-    end,
-    config = function(_, opts)
-      require("mini.map").setup(opts)
+    init = function()
+      vim.g.neominimap_singleton = true
     end,
   },
 }
