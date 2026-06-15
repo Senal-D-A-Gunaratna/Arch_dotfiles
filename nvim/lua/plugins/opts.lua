21e793589000bc5b6a32f3c25dbfd77f0fb33f0a
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-06-15 21:52:04 +0530

    fix(nvim): fix opts.lua syntax errors and remove duplicate undotree.lua

--- nvim/lua/plugins/opts.lua
+++ nvim/lua/plugins/opts.lua
@@ -9,25 +9,27 @@ return {
   },
 
   {
-  "supermaven-inc/supermaven-nvim",
-  config = function()
-    require("supermaven-nvim").setup({
-      keymaps = {
-        accept_suggestion = "<Tab>", -- Hits Tab to accept the ghost text
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-j>",
-      },
-    }),
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<Tab>", -- Hits Tab to accept the ghost text
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+      })
+    end,
   },
-{                                                                          
 
-  {"mbbill/undotree",                                   
-  cmd = "UndotreeToggle",                                                  
-  keys = {                                                                 
-    { "<leader>uu", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" }, 
-  },                                                                       
-  config = function()                                                      
-    -- Optional: Persistent undo so history survives restarts              
-    vim.opt.undofile = true                                                
-  end,                                                                     
-},                                                                         
+  {
+    "mbbill/undotree",
+    cmd = "UndotreeToggle",
+    keys = {
+      { "<leader>uu", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" },
+    },
+    config = function()
+      -- Optional: Persistent undo so history survives restarts
+      vim.opt.undofile = true
+    end,
+  },
+}
