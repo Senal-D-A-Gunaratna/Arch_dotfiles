1649d0f0018c6493d61428fd76af4efa6226931f
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-07-22 22:36:56 +0530

    nvim native

--- nvim/lua/plugins/codium.lua
+++ nvim/lua/plugins/codium.lua
@@ -1,23 +1,22 @@
 return {
-  "Exafunction/codeium.vim",
-  event = "BufEnter",
+  "Exafunction/codeium.nvim",
+  cmd = "Codeium",
+  event = "InsertEnter",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   config = function()
-    vim.g.codeium_disable_bindings = 1
-
-    vim.keymap.set("i", "<Tab>", function()
-      return vim.fn["codeium#Accept"]()
-    end, { expr = true, silent = true })
-
-    vim.keymap.set("i", "<M-]>", function()
-      return vim.fn["codeium#CycleCompletions"](1)
-    end, { expr = true, silent = true })
-
-    vim.keymap.set("i", "<M-[>", function()
-      return vim.fn["codeium#CycleCompletions"](-1)
-    end, { expr = true, silent = true })
-
-    vim.keymap.set("i", "<C-]>", function()
-      return vim.fn["codeium#Clear"]()
-    end, { expr = true, silent = true })
+    require("codeium").setup({
+      enable_cmp_source = false, -- Disables searching for hrsh7th/nvim-cmp
+      virtual_text = {
+        enabled = true,
+        key_bindings = {
+          accept = "<Tab>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+    })
   end,
 }
