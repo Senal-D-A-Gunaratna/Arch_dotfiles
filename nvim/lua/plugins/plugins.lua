d643c9ea9ab8842200e848f234ad0ab3275bf044
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-07-25 21:17:13 +0530

    moved mason config

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,4 +1,23 @@
 return {
+
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "shellcheck",
+        "debugpy",
+        "js-debug-adapter",
+        "cspell",
+        "cspell-lsp",
+        "marksman",
+        "yaml-language-server",
+        "css-lsp",
+        "taplo",
+        "pyright",
+      },
+    },
+  },
+
   {
     "folke/which-key.nvim",
     opts = {
