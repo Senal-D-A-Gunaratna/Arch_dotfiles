90d5fa8348d1d3f2426cab4f316cc34aee5eee3a
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-08-02 19:34:04 +0530

    nvim: config cleanup

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -71,4 +71,25 @@ return {
       }
     end,
   },
+
+  "Exafunction/codeium.nvim",
+  cmd = "Codeium",
+  event = "InsertEnter",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("codeium").setup({
+      enable_cmp_source = false, -- Disables searching for hrsh7th/nvim-cmp
+      virtual_text = {
+        enabled = true,
+        key_bindings = {
+          accept = "<Tab>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+    })
+  end,
 }
