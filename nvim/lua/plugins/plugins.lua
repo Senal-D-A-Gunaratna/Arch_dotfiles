2551612c14b4f1a75be1b2a093a1e7afa3788fdf
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-08-20 11:36:26 +0530

    rustfmt

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -21,6 +21,15 @@ return {
   },
 
   {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt" },
+      },
+    },
+  },
+
+  {
     "Senal-D-A-Gunaratna/swapson.nvim",
     opts = {
       npm = {
