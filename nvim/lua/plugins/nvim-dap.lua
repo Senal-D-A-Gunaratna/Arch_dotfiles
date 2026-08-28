4a148adf83af9ddfe1667576c9aaf2cd34488e23
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-08-28 20:07:02 +0530

    nvim: add nvim dap

---
+++ nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,52 @@
+return {
+  -- Core DAP
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      { "<F5>", function() require("dap").continue() end, desc = "Continue" },
+      { "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<F11>", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<F12>", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<leader>b", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>B", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Conditional Breakpoint" },
+    },
+  },
+
+  -- DAP UI (better visual experience)
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+    keys = {
+      { "<leader>du", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+    },
+  },
+
+  -- -- Debug adapters (install via Mason)
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+  --   opts = {
+  --     ensure_installed = {
+  --       "python",     -- for Python (debugpy)
+  --       "rust",       -- for Rust (lldb/codelldb)
+  --       -- "lua"      -- Lua doesn't have a standard DAP adapter
+  --     },
+  --     automatic_installation = true,
+  --   },
+  -- },
+}
