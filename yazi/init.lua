573698d5208d533ab60406e5bad243482336945a
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-09-03 18:30:04 +0530

    yazi: add gvfs mount plugin

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,1 +1,6 @@
 require("recycle-bin"):setup()
+
+require("gvfs"):setup({
+	which_keys = "1234567890qwertyuiopasdfghjklzxcvbnm-=[]\\;',./!@#$%^&*()_+{}|:\"<>?",
+	password_vault = "keyring", -- needs secret-tool + keyring, or use "pass", or nil
+})
